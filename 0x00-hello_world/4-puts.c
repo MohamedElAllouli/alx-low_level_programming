389d62932867ba0fe80d5338757d41e4b0bc1e2f
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-05 15:00:36 -0700

    cc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main
+ * reurn 0 if success
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
