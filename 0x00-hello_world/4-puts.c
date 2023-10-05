b7a1ebcba21e95dd94e7fbe8f2c8ad35bf2925ee
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-05 15:09:10 -0700

    cc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 /**
- * main function 
- * main reurn: 0 if success
+ * main - using puts function   
+ * 
+ * Decription: prints exactly Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ * reurn: 0 ii success
  */
 int main(void)
 {
