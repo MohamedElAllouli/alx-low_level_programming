1429bc54ab72c9aae1c2ae4934513855666421df
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-06 15:32:10 -0700

    task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main -conbination 
+ *
+ *Description: main function
+ *
+ *Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
