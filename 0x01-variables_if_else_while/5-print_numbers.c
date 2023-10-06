93cef4b9f65b724c1aa1501e80bed1c906644bad
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-06 13:45:29 -0700

    task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - number of dicemal base
+ *
+ *Description:main function
+ *
+ *Return: 0 if success
+ */
+int main(void)
+{	int i = 0;
+
+	for (i=0; i<10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
+
