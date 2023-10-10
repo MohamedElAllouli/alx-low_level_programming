633a3fc9776d7dab1ae72733256d348ac90c39fe
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-10 13:28:24 -0700

    task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+/**
+ *main - putchar function
+ *
+ *Return: On success 1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar('\n');
+	return (0);
+}
+
