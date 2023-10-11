c9f2597956acb7a270ddeae3325e7e2b06786fa7
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-10 17:59:34 -0700

    task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_last_digit - writes the laste digital of number
+ * @n: the number
+ *
+ * Return: last digital
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+		a = -1 * (n % 10);
+	else
+		a = n % 10;
+	_putchar(a + '0');
+	return (a);
+}
