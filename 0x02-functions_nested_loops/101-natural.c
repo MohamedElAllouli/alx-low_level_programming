995a51324451525f17deaa54fd1ec2d6cd2a4dd8
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-10 21:29:34 -0700

    task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - numbers
+ *
+ * return: 0 on success
+ */
+int main(void)
+{
+	int i;
+	int s = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+		s = s + i;
+		}
+	}
+	printf("%d\n", s);
+	return (0);
+}
