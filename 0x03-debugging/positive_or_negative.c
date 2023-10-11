372211daf8006bfb05c9c1e4d051516bb565fd8c
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-11 12:32:58 -0700

    task

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -12,14 +12,14 @@ void positive_or_negative(int n)
 {
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero", n);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative", n);
 	}
 }
