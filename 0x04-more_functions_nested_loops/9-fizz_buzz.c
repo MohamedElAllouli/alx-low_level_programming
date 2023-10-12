bd6d4e2de8de77f909db77be0a09c1a0b145efaf
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-12 16:02:27 -0700

    task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - Fizz-Buzz test
+ *
+ * Return: 1 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (1);
+}
