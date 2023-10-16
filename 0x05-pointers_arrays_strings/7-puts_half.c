9de81bbc99b63ed83decd46fb3b2d3b995fcc93d
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-16 16:11:48 -0700

    task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * puts_half - every other character of a string
+ * @s: string
+ *
+ */
+void puts_half(char *s)
+{
+	int i = 0;
+	int j = 0;
+	int n;
+
+	while (s[i] != '\0')
+	{
+		i = i + 1;
+	}
+	if (i % 2 != 0)
+	{
+		n = (i - 1) / 2;
+		for (j = n; j < i; j++)
+		{
+			_putchar(s[j]);
+		}
+	}
+	else
+	{
+		n = i / 2;
+		for (j = n; j < i; j++)
+		{
+			_putchar(s[j]);
+		}
+	}
+	_putchar('\n');
+}
