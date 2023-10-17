abea8de07e209b4433ae3729f5e8841c7d994358
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-16 17:43:09 -0700

    task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main function passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int p[100];
+	int i, s, n;
+
+	s = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		p[i] = rand() % 78;
+		s += (p[i] + '0');
+		putchar(p[i] + '0');
+		if ((2772 - s) - '0' < 78)
+		{
+			n = 2772 - s - '0';
+			s += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
