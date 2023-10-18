f8e3ae9b03353c267cd4ecfe26ed979c461b4b79
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-18 15:08:03 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,16 +11,16 @@ char *_strcat(char *s1, char *s2)
 	int i = 0;
 	int j = 0;
 
-	while (s1[i] != '\n')
+	while (s1[i] != '\0')
 	{
 		i++;
 	}
-	while (s2[j] != '\n')
+	while (s2[j] != '\0')
 	{
 		s1[i] = s2[j];
 		j++;
 		i++;
 	}
-	s1[i] = '\n';
+	s1[i] = '\0';
 	return (s1);
 }
