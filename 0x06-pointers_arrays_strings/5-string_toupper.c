c2f7962149b3d8be26f556f01b65e2ab61725719
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-18 16:46:05 -0700

    toupper string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - string to upper
+ * @s: string
+ *
+ * Return: string value
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\n')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
