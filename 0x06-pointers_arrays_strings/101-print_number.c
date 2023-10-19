883175336c144e85d3104c15e2c518bc3f7506d9
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-19 14:46:55 -0700

    task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - rot 13
+ * @s: string
+ * Return: pointer for string
+ */
+char *rot13(char *s)
+{
+	int i = 0;
+	int j;
+	char ro1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefjhijklmnopqrstuvwxyz";
+	char ro2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == ro1[j])
+			{
+				s[i] = ro2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,4 +8,9 @@ int _strcmp(char *, char *);
 void reverse_array(int *, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *rot13(char *);
+void print_number(int);
+char *infinite_add(char *, char *, char *, int);
+void print_buffer(char *, int);
+int _putchar(char);
 #endif
