096126276e39faee2be53ab243b7694b84881107
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-19 16:58:40 -0700

    task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - print number
+ *@n: number
+ *
+ */
+void print_number(int n)
+{
+	unsigned int a;
+
+	a = n;
+	if (a < 0)
+	{
+		_putchar('-');
+		a = -n;
+	}
+	if (a % 10 != 0)
+	{
+		print_number(a / 10);
+	}
+	_putchar(a % 10 + '0');
+}
