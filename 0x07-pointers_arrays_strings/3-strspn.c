d1b62480e2b8b234b3cd294a1f5d608d08e56c1b
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-23 15:55:47 -0700

    task3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - get lenght of prefix
+ * @s: string
+ * @accept: sub string
+ * Return: nuber of byte
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		for (i; accept[i]; i++)
+		{
+			if (s[j] == accept[i])
+			{
+				n++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (n);
+		}
+		j++;
+	}
+	return (n);
+}
