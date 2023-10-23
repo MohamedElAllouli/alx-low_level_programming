4db2301df754b61ae79b5f660526adf4e5251380
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-23 16:41:39 -0700

    task5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - locate substring
+ * @haystack: state string
+ * @needle: string
+ * Return: string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && h != '\0')
+		{
+			n++;
+			h++;
+		}
+		if (*h == '\0')
+			return (haystack);
+	}
+	return (0);
+}
