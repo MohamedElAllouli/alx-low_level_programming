288d7a3616a732ad389f9d6467c5b5ee08cf235e
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-23 16:19:40 -0700

    task4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#inlude "main.h"
+/**
+ * _strpbrk - searche a string
+ * @s: string source
+ * @accept: string
+ * Return: a string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j = 0;
+
+	while (s[j] != '\0')
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (s[j] == accept[i])
+			{
+				return (s);
+			}
+		}
+		j++;
+		}
+	return ('\0');
+}
