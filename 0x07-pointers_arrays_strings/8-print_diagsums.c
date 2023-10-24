01f6dc1f204966ea0e892ca3265cdb23c0887ea0
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-23 17:40:23 -0700

    task7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include<stdio.h>
-
+#include <stdio.h>
 /**
  * print_diagsums - sum of diagnals
  * @a: arry
@@ -18,7 +17,7 @@ void print_diagsums(int *a, int size)
 	}
 	for (i = size - 1; i >= 0; i--)
 	{
-		s2 = s2 + a[i * size + (size -i - 1)];
+		s2 = s2 + a[i * size + (size - i - 1)];
 	}
 	printf("%d, %d\n", s1, s2);
 }
