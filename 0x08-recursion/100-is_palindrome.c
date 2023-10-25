d54b326a1cb6f428626dbbbdfa05f30fdca8956e
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-25 15:51:01 -0700

    task100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,20 @@
 #include "main.h"
 /**
+ * _strlen_recursion - lenth of string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+	if (*s != '\0')
+	{
+		n++;
+		n = n + _strlen_recursion(s + 1);
+	}
+	return (n);
+}
+/**
  * _palindrome - is palindrom
  * @s: string
  * @i: iterator
--- 0x08-recursion/100-main.c
+++ 0x08-recursion/100-main.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -10,13 +10,13 @@ int main(void)
 {
 	    int r;
 
-	        r = is_palindrome("level");
-		    printf("%d\n", r);
-		        r = is_palindrome("redder");
-			    printf("%d\n", r);
-			        r = is_palindrome("test");
-				    printf("%d\n", r);
-				        r = is_palindrome("step on no pets");
-					    printf("%d\n", r);
-					        return (0);
+	    r = is_palindrome("level");
+	    printf("%d\n", r);
+	    r = is_palindrome("redder");
+	    printf("%d\n", r);
+	    r = is_palindrome("test");
+	    printf("%d\n", r);
+	    r = is_palindrome("step on no pets");
+	    printf("%d\n", r);
+	    return (0);
 }
