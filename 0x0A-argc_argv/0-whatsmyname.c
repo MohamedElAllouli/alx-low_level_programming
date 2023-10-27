5b409b8356b3f02ce424955c02d55e7170bfd709
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-27 16:06:36 -0700

    task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * main - main function
  * @argv: content of commande line
  * @argc: argc
- * Return: 0 olwayse in success
+ * Return: argc olwayse in success
  */
 int main(int argc, char *argv[])
 {
