9c19f56143758ec5af48631dca0815c5ff92b881
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-27 15:55:54 -0700

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argv: content of commande line
+ * @argc: argc
+ * Return: 0 olwayse in success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
