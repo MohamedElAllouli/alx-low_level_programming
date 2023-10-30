40f7df71fbc29f32c5f8afd439dbcfd98f621ef0
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-10-30 13:39:29 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return (s);
+		return (NULL);
 	s = malloc(sizeof(*s) * size);
 	for (i = 0; i < size; i++)
 	{
