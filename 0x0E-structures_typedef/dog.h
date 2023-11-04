f6c64efe4e2e75ca777e710a410d8edcddb7434b
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-11-04 15:17:56 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+/**
+ * struct dog - dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Dog information
+ */
+ struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
