c4479cfdc1b86ac787b23609006409ab1c7a13b0
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-11-08 16:09:51 -0800

    all tasks

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_name(char *, void (*)(char *));
+void print_name(char *, void (*f)(char *));
 void array_iterator(int *, size_t, void (*)(int));
 int int_index(int *, int, int (*)(int));
 
