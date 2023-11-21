c91d2d968fce3bf1052e38c98b04977aa0e9f441
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-11-21 03:42:38 -0800

    task6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,7 +10,7 @@ int pop_listint(listint_t **head)
 	listint_t *tmp = NULL;
 	int n;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 	n = (*head)->n;
 	tmp = (*head)->next;
