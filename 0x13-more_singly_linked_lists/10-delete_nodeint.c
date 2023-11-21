ae94846ab96b9c17c3a83ee268da0d133f093da6
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-11-21 07:51:48 -0800

    task10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,6 +13,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 
 	p = *head;
+	if (p == NULL)
+		return (-1);
 	if (index == 0)
 	{
 		(*head) = (*head)->next;
