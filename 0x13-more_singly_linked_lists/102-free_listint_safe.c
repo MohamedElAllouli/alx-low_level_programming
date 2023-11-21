2ad174493b01b2e5dda435063e95273e1dc05ca3
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-11-21 11:18:11 -0800

    task102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,83 @@
+#include "lists.h"
+
+/**
+ * listint_count - Counts the number of unique nodes
+ * @head: the head of list
+ * Return: size of list - 0.
+ */
+size_t listint_count(listint_t *head)
+{
+	listint_t *t, *h;
+	size_t n = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	t = head->next;
+	h = (head->next)->next;
+	while (h)
+	{
+		if (t == hare)
+		{
+			t = head;
+			while (t != h)
+			{
+				n++;
+				t = t->next;
+				h = h->next;
+			}
+			t = t->next;
+			while (t != h)
+			{
+				n++;
+				t = t->next;
+			}
+
+			return (n);
+		}
+
+		t = t->next;
+		h = (h->next)->next;
+	}
+
+	return (0);
+}
+
+/**
+ * free_listint_safe - Frees a list
+ * @h: head of list
+ * Return: size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmp;
+	size_t n, i;
+
+	n = listint_count(*h);
+
+	if (n == 0)
+	{
+		for (; h != NULL && *h != NULL; n++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+	}
+
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (n);
+}
