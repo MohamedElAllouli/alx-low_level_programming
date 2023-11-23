39664f912426137ff9edc89596e87cc306d2b401
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2023-11-23 14:20:57 -0800

    task100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 or  1  endian
+ */
+int get_endianness(void)
+{
+	int i;
+	char *c;
+
+	i = 1;
+	c = (char *)&i;
+	return (*c);
+}
