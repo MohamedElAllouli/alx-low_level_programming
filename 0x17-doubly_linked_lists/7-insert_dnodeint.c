eefa5d553728d83da1b65f0c1f726b8b21aabffa
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-01-11 15:48:11 -0800

    task7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,6 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new = NULL, *tmp = *h;
-	unsigned int i = 0;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
