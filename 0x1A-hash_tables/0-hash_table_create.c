81e394cce73e437d09cae693072c00228b78cdf5
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-02-02 11:33:06 -0800

    hash

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create hash table
+ * @size: size of table
+ *
+ * Return: hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+	unsigned long i = 0;
+
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_s *) * size));
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		new->array[i] = NULL;
+	return (new);
+}
